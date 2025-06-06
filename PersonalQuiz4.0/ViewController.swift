1379ba9c12650dc1600e7fc32195ccae1f1d1ab1
mikle <mikle.kontinent@gmail.com>
2025-06-06 18:59:51 +0700

    Initial Commit

---
+++ PersonalQuiz4.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQuiz4.0
+//
+//  Created by Таня Кожевникова on 06.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
